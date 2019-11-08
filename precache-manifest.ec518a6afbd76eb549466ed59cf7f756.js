e62532d2ef1bf7bc08e739a3c63eceb86e41c731
Viktoriia <jhunt@ukr.net>
2019-11-08 20:18:40 +0200

    Updates

---
+++ precache-manifest.ec518a6afbd76eb549466ed59cf7f756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3bef46be747a267f01c8f4a46029f32",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "eee06fd52436a7f21eec",
+    "url": "/react_tabs-with-router/static/css/main.6b76eb3c.chunk.css"
+  },
+  {
+    "revision": "1d3739fe0b2e602d36e9",
+    "url": "/react_tabs-with-router/static/js/2.76fffd90.chunk.js"
+  },
+  {
+    "revision": "eee06fd52436a7f21eec",
+    "url": "/react_tabs-with-router/static/js/main.b0e8a879.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
